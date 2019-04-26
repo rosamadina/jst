dfceea4d9d57ad71ef0b1c06a64486d0342972e2
rosamadina <49849060+rosamadina@users.noreply.github.com>
2019-04-26 22:36:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,76 @@
 import numpy as np
 import pandas as pd
 
-r = 0
-alpha = 0.6
-beta = 0.5
-w = [[0.2, 0.6, 0.5, 0.9],
-     [0.8,0.4,0.7,0.3]]
+class som:
+    def __init__(self, alpha,r,beta,et):
+        self.alpha =alpha
+        self.r = r
+        self.beta = beta
+        self.et = et
+        # self.epoch = 1
+        # self.w = [[0.3, 0.9, 0.7, 0.2],
+        #           [0.1,0.5,0.8,0.4],
+        #           [0.9,0.7,0.3,0.4],
+        #           [0.1,0.5,0.8,0.3]]
 
-def readcsv(filename):
-    data = pd.read_csv(filename)
-    return (np.array(data))
+        self.w = [[0.2, 0.6, 0.5, 0.9],
+                  [0.8,0.4,0.7,0.3]]
 
-def neuronPemenang(data):
-    temp = [0]*len(data)
-    for i in range(len(data)):
-        temp2 = np.zeros((len(w)))
-        for j in range(len(w)):
-            for k in range(len(w[j])):
-                temp2[j] += pow(np.subtract(w[j][k] , data[i][k]), 2)
-            index = np.argmin(temp2)
-            temp[i]= index #index neuron pemenang
-    return temp
+        # self.w = np.random.rand(2,23)
 
-# def R():
-#     if R == 0:
-#         updateBobot()
-#     else:
+    def readcsv(self, filename):
+        self.data = pd.read_csv(filename)
+        return (np.array(self.data))
 
-def updateBobot(data, index):
-    temps = [0] * len(data)
-    j = 0
-    for i in range(len(data)):
-        for j in range(j, j+4):
-            temp = index[j]
-            if r == 0:
-                w[temp] = np.add(w[temp], alpha * np.array((np.subtract(data[i], w[temp]))))
-            # break
-            else:
-                w[temp] = np.add(w[temp], alpha * np.array((np.subtract(data[i], w[temp]))))
-        j+=1
-        temps[i] = w[temp]
-    return temps
+    def neuronPemenang(self,data):
+        temp = [0]*len(data)
+        temps = [0]*len(self.w)
+        for i in range(len(data)):
+            temp2 = [0]*len(self.w)
+            for j in range(len(self.w)):
+                for k in range(len(self.w[j])):
+                    temp2[j] += pow(np.subtract(self.w[j][k] , data[i][k]), 2)
+                index = np.argmin(temp2)
+                temp[i]= index #index neuron pemenang
+        return temp
 
-array = readcsv('coba.csv')
-index = neuronPemenang(array)
-print("array\n",array)
-print("neuron pemenang\n",index)
-print("update bobot \n", updateBobot(array,index))
+    def TuningPhase(self, data, index): #update bobot neuron pemenenag
+        temps = [0] * len(data)
+        j = 0
+        for i in range(len(data)):
+            for j in range(j, j+len(index)):
+                temp = index[j]
+                self.w[temp] = np.add(self.w[temp], self.alpha * np.array((np.subtract(data[i], self.w[temp]))))
+                break
+            j+=1
+            temps[i] = self.w[temp]
+        return temps
 
+    def OrderingPhase(self, data, index): #update bobot neuron pemenang dan tetangga
+        return
 
-
-
-
-
-
-
+    def updateBobot(self):
+        data = self.readcsv('coba.csv')
+        index = self.neuronPemenang(data)
+        if self.r == 0:
+            bobot = self.TuningPhase(data, index)
+        elif self.r>0:
+            bobot= self.OrderingPhase(data,index)
+        else:
+            print("error")
+        return bobot
 
 
+    def berhenti(self):
+        data = self.readcsv('coba.csv')
+        index = self.neuronPemenang(data)
+        epoch = 1
+        for i in range(self.et):
+            if epoch <= self.et:
+                b = self.neuronPemenang(data)
+                a = self.updateBobot()
+                print('epoch', epoch, ': \n', 'neuron pemenang :', b, '\n update bobot: \n',a)
+                epoch += 1
+                self.alpha *= self.beta
+                print("alpha: ",self.alpha)
+        return ""
